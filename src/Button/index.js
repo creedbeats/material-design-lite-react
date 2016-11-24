bc2a8f71987b3b08f98be28d9fd4d1ee70afa54a
Christopher Reed <Christopher Reed>
2016-11-23 21:11:31 -0500

    added button component

---
+++ src/Button/index.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import classNames from 'classnames';
+
+export default class Button extends Component {
+  constructor(){
+    super();
+
+    const {
+      children,
+      colored = false,
+      ripple = false,
+      fab = false,
+    } = this.props;
+
+    this.props.className = classNames('mdl-button mdl-js-button', {
+      'mdl-button--fab': fab || FAB,
+      'mdl-button--colored': colored,
+      'mdl-js-ripple-effect': ripple,
+      'mdl-button--raised': raised,
+      'mdl-button--accent': accent,
+      'mdl-button--primary': primary,
+      'mdl-button--icon': icon,
+      'mdl-button--fab mdl-button--mini-fab': miniFab || miniFAB;
+    }, this.props.className);
+  }
+
+  render() {
+    return (
+      <button {...this.props}>
+        {children}
+      </button>
+    );
+  }
+}
